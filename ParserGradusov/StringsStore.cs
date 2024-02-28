33b50658d259b8087b9c5f6baf2551c9c5fc41ce
Dmitrii Zaitsev <as.aeasss@mail.ru>
2024-02-28 22:57:40 +0300

    .

--- ParserGradusov/StringsStore.cs
+++ ParserGradusov/StringsStore.cs
@@ -2,7 +2,7 @@
 {
     public class StringsStore
     {
-        public const string Filepath = "D:\\C#proj\\ParserGradusov\\ParserGradusov\\3328496339_40702810010000001390_002_0519.txt";
+        public const string Filepath = @"D:\C#proj\ParserGradusov\ParserGradusov\3328496339_40702810010000001390_002_0519.txt";
         public const string MongoDbCollectionName = "usersinfo";
         public const string MongoDbDatabaseName = "ParserUserInfo";
         public const string MongoDbConnectionString = "mongodb://localhost:27017/?directConnection=true";
